eacfefb0c1bd3f08259c450fa6199b5e49caf4e0
김수지 <choyen2727@naver.com>
2025-02-18 12:58:48 +0900

    static 수정

--- src/main/resources/static/assets/NotificationIndex-DdSbeEmq.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-C6bOqW8w.js";const o=()=>t.jsx("div",{children:"NotificationIndex"});export{o as default};
--- src/main/resources/static/assets/User-DPWyo1XD.js
+++
@@ -1,1 +0,0 @@
-import{j as s,O as t}from"./index-C6bOqW8w.js";const e=()=>s.jsx("div",{children:s.jsx(t,{})});export{e as default};
