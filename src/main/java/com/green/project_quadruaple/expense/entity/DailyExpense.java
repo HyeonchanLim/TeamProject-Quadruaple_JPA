cb8ed2be420cabe7a2e7f96c56956724f9514c25
김수지 <choyen2727@naver.com>
2025-02-24 13:01:03 +0900

    가계부 entity 작성

---
+++ src/main/java/com/green/project_quadruaple/expense/entity/DailyExpense.java
@@ -0,0 +1,12 @@
+package com.green.project_quadruaple.expense.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class DailyExpense {
+}
