d4ec7a0dde5081f84e0a87e4a68fe54badbbcd37
unknown <wjddnr1gh@naver.com>
2025-02-25 18:24:09 +0900

    socket 작업중

--- src/main/java/com/green/project_quadruaple/chat/service/ChatService.java
+++ src/main/java/com/green/project_quadruaple/chat/service/ChatService.java
@@ -1,16 +1,55 @@
 package com.green.project_quadruaple.chat.service;
 
+import com.green.project_quadruaple.chat.model.ChatDto;
+import com.green.project_quadruaple.chat.model.JoinReq;
+import com.green.project_quadruaple.chat.model.JoinRes;
+import com.green.project_quadruaple.common.config.jwt.JwtUser;
+import com.green.project_quadruaple.common.config.security.AuthenticationFacade;
+import com.green.project_quadruaple.entity.model.User;
+import com.green.project_quadruaple.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChatService {
 
-    public String save() {
+    private final UserRepository userRepository;
 
+    @Transactional
+    public JoinRes joinChat(JoinReq req, Principal principal) {
+
+        Long signedUserId = getSignedUserId(principal);
+        if(signedUserId == null) { // 인증 정보 없으면 return
+            return JoinRes.builder()
+                    .userName(null)
+                    .error("유저 정보 없음")
+                    .build();
+        }
+        User user = userRepository.findById(signedUserId).get();
+        return JoinRes.builder()
+                .userName(user.getName())
+                .error(null)
+                .build();
+    }
+
+    public String insChat(ChatDto req) {
+        return null;
+    }
+
+    private Long getSignedUserId(Principal principal) {
+        Authentication authentication = (Authentication) principal;
+        if (authentication.getPrincipal() instanceof JwtUser) {
+            return ((JwtUser)authentication.getPrincipal()).getSignedUserId();
+        }
         return null;
     }
 }
--- src/main/java/com/green/project_quadruaple/common/config/socket/WebSocketConfig.java
+++ src/main/java/com/green/project_quadruaple/common/config/socket/WebSocketConfig.java
@@ -1,15 +1,23 @@
 package com.green.project_quadruaple.common.config.socket;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompChannelInterceptor stompChannelInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -26,6 +34,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(new StompChannelInterceptor());
+        registration.interceptors(stompChannelInterceptor);
     }
 }
