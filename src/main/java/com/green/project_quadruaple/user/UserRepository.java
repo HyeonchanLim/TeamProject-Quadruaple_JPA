2b69e41ba36f9f5c8eaf076398ef22f72cf930e3
SongEunA <dmsdktt@gmail.com>
2025-02-26 14:53:51 +0900

    소셜 로그인 LazyInitializationException 수정

--- src/main/java/com/green/project_quadruaple/user/UserRepository.java
+++ src/main/java/com/green/project_quadruaple/user/UserRepository.java
@@ -2,11 +2,13 @@ package com.green.project_quadruaple.user;
 
 import com.green.project_quadruaple.common.config.security.SignInProviderType;
 import com.green.project_quadruaple.entity.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "authenticationCode")
     User findByAuthenticationCode_EmailAndProviderType(String email, SignInProviderType providerType);
 
     Optional<User> findByAuthenticationCode_Email(String email);
