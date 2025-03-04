938d283c72f411fbf3d45c13f0d121fecf7030e1
SongEunA <dmsdktt@gmail.com>
2025-03-04 19:03:13 +0900

    PointCard update 작업

--- src/main/java/com/green/project_quadruaple/pointcard/PointCardRepository.java
+++ src/main/java/com/green/project_quadruaple/pointcard/PointCardRepository.java
@@ -3,5 +3,5 @@ package com.green.project_quadruaple.pointcard;
 import com.green.project_quadruaple.entity.model.PointCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PointCardRepository extends JpaRepository<PointCard, Long> {
+public interface PointCardRepository extends JpaRepository<PointCard, String> {
 }
