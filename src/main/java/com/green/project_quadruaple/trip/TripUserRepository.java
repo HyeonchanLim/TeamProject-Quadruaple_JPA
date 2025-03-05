86853678fa2022c5b4bf7e2239932a3ac68a192a
김수지 <choyen2727@naver.com>
2025-03-05 15:42:18 +0900

    실시간 알림과 여행 개설시 알람발송 메서드

--- src/main/java/com/green/project_quadruaple/trip/TripUserRepository.java
+++ src/main/java/com/green/project_quadruaple/trip/TripUserRepository.java
@@ -1,5 +1,6 @@
 package com.green.project_quadruaple.trip;
 
+import com.green.project_quadruaple.entity.model.Trip;
 import com.green.project_quadruaple.entity.model.TripUser;
 import com.green.project_quadruaple.entity.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,6 @@ import java.util.List;
 public interface TripUserRepository extends JpaRepository<TripUser, Long> {
     boolean existsByUser_userIdAndTrip_tripIdAndDisable(Long userId, Long tripId, int disable);
 
-    @Query("select tu.user from TripUser tu where tu.trip.tripId=:tripId")
-    List<User> findUserByTripId(Long tripId);
+    @Query("select tu.user from TripUser tu where tu.trip=:trip")
+    List<User> findUserByTrip(Trip trip);
 }
