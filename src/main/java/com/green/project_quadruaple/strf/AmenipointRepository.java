af648231dd7874d7c12773b8891b5b96892b12f6
LimHyeonChan <lhc0240@gmail.com>
2025-03-13 15:34:20 +0900

    redis 설정

--- src/main/java/com/green/project_quadruaple/strf/AmenipointRepository.java
+++ src/main/java/com/green/project_quadruaple/strf/AmenipointRepository.java
@@ -5,6 +5,10 @@ import com.green.project_quadruaple.entity.model.AmenipointId;
 import com.green.project_quadruaple.entity.model.StayTourRestaurFest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AmenipointRepository extends JpaRepository<Amenipoint , AmenipointId> {
     void deleteByStayTourRestaurFest(StayTourRestaurFest strf);
+    List<AmenipointId> findAllByAmenityIdAndStrfId(List<Long> amenityIds , Long strfId);
+    List<Amenipoint> findAllByAmeniPointId(Long ameniPointId);
 }
