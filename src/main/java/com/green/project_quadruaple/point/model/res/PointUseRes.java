1ad8ad0896b640d09fd038de01e5584d2506d51b
김수지 <choyen2727@naver.com>
2025-03-13 16:08:39 +0900

    포인트 사용 반환값 추가

---
+++ src/main/java/com/green/project_quadruaple/point/model/res/PointUseRes.java
@@ -0,0 +1,9 @@
+package com.green.project_quadruaple.point.model.res;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class PointUseRes {
+    private int remainPoints;
+    private int usedPoints;
+}
