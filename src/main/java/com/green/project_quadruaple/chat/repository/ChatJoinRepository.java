36500bae8f9daabd0441024a545afe25b64cc8ec
unknown <wjddnr1gh@naver.com>
2025-03-14 20:39:15 +0900

    관리자 생성 완료

--- src/main/java/com/green/project_quadruaple/chat/repository/ChatJoinRepository.java
+++ src/main/java/com/green/project_quadruaple/chat/repository/ChatJoinRepository.java
@@ -1,6 +1,7 @@
 package com.green.project_quadruaple.chat.repository;
 
 import com.green.project_quadruaple.entity.model.ChatJoin;
+import com.green.project_quadruaple.entity.model.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -27,4 +28,20 @@ public interface ChatJoinRepository extends JpaRepository<ChatJoin, Long> {
         where cj.chatRoom.chatRoomId = :roomId
         """)
     List<ChatJoin> findChatJoinIdListByChatRoomId(Long roomId);
+
+    @Query("""
+        SELECT cr
+        FROM ChatRoom cr
+        WHERE cr.chatRoomId IN (
+            SELECT cj2.chatRoom.chatRoomId
+            FROM ChatJoin cj2
+            WHERE cj2.user.userId = :hostUserId
+        )
+        AND cr.chatRoomId IN (
+            SELECT cj3.chatRoom.chatRoomId
+            FROM ChatJoin cj3
+            WHERE cj3.user.userId = :inviteUserId
+        )
+        """)
+    List<ChatRoom> findChatJoinsByUserIds(Long hostUserId, Long inviteUserId);
 }
