84de542195fb8dba8e92b9e4ceb08167c784a204
김수지 <choyen2727@naver.com>
2025-03-18 10:27:31 +0900

    notice 연결 pool문제개선

--- src/main/java/com/green/project_quadruaple/notice/NoticeRepository.java
+++ src/main/java/com/green/project_quadruaple/notice/NoticeRepository.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
-    @Query(value = "SELECT DISTINCT user_id FROM notice_event_log WHERE event_time > :lastCheckedTime", nativeQuery = true)
+    @Query(value = "SELECT user_id FROM notice_event_log WHERE event_time > :lastCheckedTime group by user_id", nativeQuery = true)
     List<Long> getUsersWithNewNotices(LocalDateTime lastCheckedTime);
     @Transactional
     @Modifying
